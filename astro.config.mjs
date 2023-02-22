0217e88391009795d24728a0f3c84d9f6055f937
Thomas Whitaker <tom@tomwritescode.com>
2023-02-23 10:49:21 +1100

    vercel/serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react'
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   integrations: [react()]
 });
