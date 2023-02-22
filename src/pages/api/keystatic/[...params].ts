14a3e4b5d461d8e79829908710306d999d116db2
Emma Hamilton <git@emmas.town>
2023-02-22 16:22:44 +1000

    Try a thng

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -1,11 +1,16 @@
 // /pages/api/keystatic/[...params].ts
 
-import { makeHandler } from '@keystatic/astro/api'
-import config from '../../../../keystatic.config'
+import { makeHandler } from "@keystatic/astro/api";
+import config from "../../../../keystatic.config";
 
-export const all = makeHandler({
+const handler = makeHandler({
   config,
   secret: import.meta.env.KEYSTATIC_SECRET,
   clientId: import.meta.env.KEYSTATIC_GITHUB_CLIENT_ID,
   clientSecret: import.meta.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
-})
+});
+
+export const all: typeof handler = (context) => {
+  console.log(import.meta.env.KEYSTATIC_GITHUB_CLIENT_ID);
+  return handler(context);
+};
