317a2b24f5e9d0420f9312818b6be28e1fb0382a
Thomas Whitaker <tom@tomwritescode.com>
2023-02-24 14:59:10 +1100

    import.meta -> process.env

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import react from '@astrojs/react'
 import vercel from '@astrojs/vercel/serverless';
 import node from '@astrojs/node'
 
-// const isVercelBuild = import.meta.env.CI || import.meta.env.VERCEL
+const isVercelBuild = process.env.CI || process.env.VERCEL
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: isVercelBuild ? vercel(): node({mode: 'standalone'}),
   integrations: [react()]
 });
